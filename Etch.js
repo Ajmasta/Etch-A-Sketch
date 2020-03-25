0971ccb2c73dc1a41f863a7b01759280ef4a1220
ajmasta <adrien.moevus@mail.mcgill.ca>
2020-03-24 21:53:44 -0400

    Added color choices

--- Etch.js
+++ Etch.js
@@ -1,7 +1,19 @@
 let grid = document.querySelector(".grid")
 let root= document.documentElement
 let reset = document.querySelector(".button")
+let color = document.getElementById("color")
+let colorInput = false
+let colorChoice = "#ffc600"
+function colorHandler(e){
+    e.target.value++
 
+    let red = Math.floor(Math.random()*256)
+    let green = Math.floor(Math.random()*256)
+    let blue= Math.floor(Math.random()*256)
+
+    e.target.style.backgroundColor = `rgba(${red},${green},${blue},${1-e.target.value /10})`
+    if(colorInput) e.target.style.backgroundColor = `${colorChoice}`
+}
 function fillGrid(row){
     grid.innerHTML=""
     for(i=1; i<=row;i++){
@@ -9,20 +21,10 @@ function fillGrid(row){
         let div = document.createElement("div")
         div.value = 0
         div.className = "coloredGrid"
-        div.addEventListener("mouseover", e =>{
-            
-            e.target.value++
-
-            let red = Math.floor(Math.random()*256)
-            let green = Math.floor(Math.random()*256)
-            let blue= Math.floor(Math.random()*256)
-
-
-            e.target.style.backgroundColor = `rgba(${red},${green},${blue},${1-e.target.value /10})`
-            })
-
+        div.addEventListener("mouseover", colorHandler)
             grid.appendChild(div)
         }
+        
     }
 }
 
@@ -36,7 +38,7 @@ document.querySelectorAll(".coloredGrid").forEach(div=> {
 
 document.getElementById("gridSize").addEventListener("click", ()=>{
         
-        let sizeOfGrid = prompt("Enter the number of cases you want (<150)")
+        let sizeOfGrid = prompt("Enter the grid size you want (<150)")
         if (sizeOfGrid >= 200) alert("Careful! We do not support more than 150 cases. But you can try...")
         
         fillGrid(sizeOfGrid)
@@ -44,4 +46,8 @@ document.getElementById("gridSize").addEventListener("click", ()=>{
 
 })
 
+color.addEventListener("change", e=>{
+    colorInput = true;
+    colorChoice = e.target.value;
+} )
 fillGrid(32)
